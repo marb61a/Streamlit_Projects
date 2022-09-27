c64734be3eee8090b36fb6a12737a98b7b34ed6f
Martin <marb61a@gmail.com>
2022-09-27 14:24:54 +0100

    Updated app.py to display sliders, selects, images and videos

--- streamlitBasics/app.py
+++ streamlitBasics/app.py
@@ -2,26 +2,33 @@
 import streamlit as st
 
 # Working with widgets
-# Includes buttons, radio buttons, checkboxes and selects
-name = "Martin"
-if st.button("Submit"):
-    st.write("Name: {}".format(name.upper()))
+# Includes selects, sliders and multi sliders
+# Select and multiple select
+my_lang = ["Python", "JavaScript", "Java", "C#", "SQL"]
+choice = st.selectbox("Language: ", my_lang)
+st.write("You selected {}".format(choice))
 
-# Removes error from using button with the same values
-if st.button("Submit", key='new02'):
-    st.write("Name: {}".format(name.lower()))
+# Multiple selection
+my_lang_multi = ["Python", "JavaScript", "Java", "C#", "SQL"]
+multi_choice = st.multiselect("Multiple languages ", my_lang_multi , default="JavaScript")
 
-# Radio buttons
-status = st.radio("What is your status", ("Active", "Inactive"))
-if status == "Active":
-    st.success("You are active")
-else:
-    st.warning("You are inactive")
+# Slider
+# Numerical datatypes only
+age = st.slider("Age", 1, 100)
 
-# Checkboxes
-if st.checkbox("Show/Hide"):
-    st.text("Showing Info")
+# Select Slider
+# Any datatype
+color = st.select_slider("Choose Color", options=["Red", "Blue", "Green", "Yellow", "Black", "Purple"])
 
-# Beta Expander
-with st.expander("Python"):
-    st.success("Hello Python")
+# Work with media files such as images, video etc
+from PIL import Image
+img = Image.open("./ignore.jpg")
+# Displayes image using full width
+st.image(img, use_column_width=True)
+
+# Import image from url
+st.image("https://www.simplilearn.com/ice9/free_resources_article_thumb/Why-get-certified-in-Artificial-Intelligence.jpg")
+
+# Display videos
+# video_file = open("", "rb").read()
+# st.video(video_file)
