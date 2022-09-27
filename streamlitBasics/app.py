8c57c08a9f1170bf8ea727fefe65c1533f6243f6
Martin <marb61a@gmail.com>
2022-09-27 13:46:57 +0100

    Moved table related commands to new file to update app.js

--- streamlitBasics/app.py
+++ streamlitBasics/app.py
@@ -1,23 +1,3 @@
 # Core packages
 import streamlit as st
 
-# Additional packages
-import pandas as pd
-
-# Display data
-df = pd.read_csv("./diabetes_data_upload.csv")
-
-# Show entire dataframe
-st.dataframe(df, 200)
-
-# Add a color style from pandas
-st.dataframe(df.style.highlight_max(axis=0))
-
-# Static Table
-# st.table(df)
-
-# Using superfunction st.write
-st.write(df.head())
-
-# Display Json
-st.json({'data':'name'})
---
+++ streamlitBasics/app_displaying_tables.py
@@ -0,0 +1,31 @@
+# Core packages
+import streamlit as st
+
+# Additional packages
+import pandas as pd
+
+# Display data
+df = pd.read_csv("./diabetes_data_upload.csv")
+
+# Show entire dataframe
+st.dataframe(df, 200)
+
+# Add a color style from pandas
+st.dataframe(df.style.highlight_max(axis=0))
+
+# Static Table
+# st.table(df)
+
+# Using superfunction st.write
+st.write(df.head())
+
+# Display Json
+st.json({'data':'name'})
+
+# Display Code
+mycode = """
+    def sayhello():
+        print('Hello Streamlit Fans')
+"""
+
+st.code(mycode)
