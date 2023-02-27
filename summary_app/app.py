ae05250d3b4717cf4845e70fa93d43be41b0f737
Martin <marb61a@gmail.com>
2023-02-27 22:05:19 +0000

    Updated app.py and installed needed packages

--- summary_app/app.py
+++ summary_app/app.py
@@ -2,6 +2,22 @@
 import streamlit as st
 
 # Any additional packages
+# TextRank Algorithm
+from gensim.summarization import summarize
+
+# LexRank Algorithm
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+# EDA Pakages
+import pandas as pd
+
+# data Visualisation
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib
+matplotlib.use("Agg")
 
 def main():
     # A simple summarisation app
@@ -16,6 +32,14 @@ def main():
         st.subheader("Summarisation")
         # Create an area to enter text
         raw_text = st.text_area("Enter Text Here")
+
+        if st.button("Summarise"):
+            # st.write(raw_text)
+            with st.beta_expander("Original Text"):
+                st.write(raw_text)
+            
+            # Layout
+            
     else:
         st.subheader("About")
 
