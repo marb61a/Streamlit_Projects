77b95633794ad5c93fd518de954c07e5e6d3428c
Martin <marb61a@gmail.com>
2023-02-28 12:30:30 +0000

    Updated app.py with text summary function

--- summary_app/app.py
+++ summary_app/app.py
@@ -19,6 +19,15 @@ import seaborn as sns
 import matplotlib
 matplotlib.use("Agg")
 
+# Function for Sumy Summarization
+def sumy_summarizer(docx, num=2):
+    parser = PlaintextParser.from_string(docx, Tokenizer("english"))
+    lex_summarizer = LexRankSummarizer()
+    summary = lex_summarizer(parser.document, num)
+    summary_list = [str(sentence) for sentence in summary]
+    result = ''.join(summary_list)
+    return result
+
 def main():
     # A simple summarisation app
     st.title("Summarising App")
@@ -39,6 +48,16 @@ def main():
                 st.write(raw_text)
             
             # Layout
+            c1, c2 = st.beta_columns(2)
+            with c1:
+                with st.beta_expander("LexRank Summary"):
+                    my_summary = sumy_summarizer(raw_text)
+                    st.write(my_summary)
+                    
+            with c2:
+                with st.beta_expander("TextRank Summary"):
+                    my_summary = summarize(raw_text)
+                    st.write(my_summary)    
             
     else:
         st.subheader("About")
