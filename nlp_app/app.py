10ff6a5429d533fada6baa059190e68a94675fb8
Martin <marb61a@gmail.com>
2023-03-01 12:02:01 +0000

    Ypdated app.py with basic strcutral elements

--- nlp_app/app.py
+++ nlp_app/app.py
@@ -5,5 +5,30 @@ import streamlit as st
 def main():
     st.title("NLP Streamlit App")
 
+    # Add menu items
+    menu = ["Home", "NLP(files)", " About"]
+
+    # Add sidebar
+    choice = st.sidebar.selectbox("Menu", menu)
+    if choice == "Home":
+        st.subheader("Home: Analyse Text")
+
+        # Add a text area
+        raw_text = st.text_area("Enter Text Here")
+        st.write(raw_text)
+
+        # Add sidebar selection for most common words (Tokens)
+        num_of_most_common = st.sidebar.number_input("Most Common Tokens", 5, 15)
+
+        # Add button under text area
+        if st.button("Analyse"):
+            with st.beta_expander("Original Text"):
+                st.write(raw_text)
+                
+    elif choice == "NLP(files)":
+        st.subheader("NLP Task")
+    else:
+        st.subheader("About")
+
 if __name__ == "__main__":
     main()
